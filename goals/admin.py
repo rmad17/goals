e36dcdc50bbfd116e38c83f48521e6a92157e29e
rmad17 <souravbasu17@gmail.com>
2016-09-08 10:03:18 +0530

    Added model with support in admin panel

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 rmad17 <souravbasu17@gmail.com>
+#
+# Distributed under terms of the MIT license.
 
-# Register your models here.
+"""
+Admin Panel related functionality
+"""
+
+from django.contrib import admin
+from .models import Goal
+
+
+admin.site.register(Goal)
--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Goal(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=100)
+    target_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ('created_at',)
+        verbose_name = "Life Goal"
+
+    def as_dict(self):
+        return {'uuid': self.uuid, 'title': self.title, 'target_date':
+                self.target_date, 'end_date': self.end_date}
+
+    def __str__(self):
+        return self.title
