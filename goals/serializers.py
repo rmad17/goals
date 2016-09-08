3ebf500b908fef69400e977a7495a5be5cbc773b
rmad17 <souravbasu17@gmail.com>
2016-09-08 08:12:29 +0530

    Added serializerss

---
+++ goals/serializers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 rmad17 <souravbasu17@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Serializers for Django Rest Framework
+"""
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
--- life/settings.py
+++ life/settings.py
@@ -92,6 +92,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
